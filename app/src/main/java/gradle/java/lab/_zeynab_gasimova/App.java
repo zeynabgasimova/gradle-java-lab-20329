8e1484a6378514b09d18d66aa5a8f323541143ee
zeynabgasimova <zgasimova17998@ada.edu.az>
2026-01-31 23:18:00 +0400

    Initial Gradle Java project setup

---
+++ app/src/main/java/gradle/java/lab/_zeynab_gasimova/App.java
@@ -0,0 +1,12 @@
+package gradle.java.lab._zeynab_gasimova;
+
+public class App {
+
+    public String getGreeting() {
+        return "Gradle build and Git workflow successful.";
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Gradle build and Git workflow successful.");
+    }
+}

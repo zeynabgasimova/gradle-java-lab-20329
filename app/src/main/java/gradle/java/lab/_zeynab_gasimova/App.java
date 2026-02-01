5ebe07bb78a17a4c75b9efd7b9004ad3eab7122a
vasilaAliyeva <vasilaaliyeffa@gmail.com>
2026-02-01 22:31:07 +0400

    Student B successful commit

--- app/src/main/java/gradle/java/lab/_zeynab_gasimova/App.java
+++ app/src/main/java/gradle/java/lab/_zeynab_gasimova/App.java
@@ -4,6 +4,9 @@ public class App {
 
 
     public static void main(String[] args) {
+        System.out.println("Hello! This is Student B working on Task 6 of the Git workflow.");
         System.out.println("Student B: Vasila Aliyeva");
+        System.out.println("This message confirms that the feature/message-update branch changes are applied.");
+       
     }
 }
